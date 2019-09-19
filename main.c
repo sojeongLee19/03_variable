2c3b1a33f5f3528c063a9c758c12fa5e60242633
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-09-19 16:07:14 +0900

    02

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+         int x;
+         short y;
+         float z;
+         double a;
+         
+	printf("bytes of int : %d/n" , sizeof(x) );
+	printf("bytes of short : %d/n" , sizeof(y) );
+		
+	printf("bytes of float : %d/n" , sizeof(z) );
+	printf("bytes of double : %d/n" , sizeof(a) );
+		
+	return 0;
+}
+
